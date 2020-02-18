2f8dd09d40a76745e3ef8b983e96c38a4e5c4eb0
Humberto Acea <humbertoacea@gmail.com>
2020-02-18 12:59:47 +0100

    Delete config.js

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyCU1FSLcn_TXCvJNYcWie3358eTyjs5IeI',
-  authDomain: 'tidal-anvil-197316.firebaseapp.com',
-  databaseURL: 'https://tidal-anvil-197316.firebaseio.com',
-  projectId: 'tidal-anvil-197316',
-  storageBucket: 'tidal-anvil-197316.appspot.com',
-  messagingSenderId: '1061203455477',
-  appId: '1:1061203455477:web:5e92a12a324729ef5984bc'
-};
